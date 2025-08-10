c945decc95a93182ff1bbb007eaa9ab380e935fb
kiganyamburu <mburukiganya@gmail.com>
2025-08-10 21:20:46 +0300

    feat(App): work on the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 
 import ProductPage from "./pages/ProductPage";
+
+import NotFound from "./pages/NotFound";
 
 import CategoriesPage from "./pages/CategoriesPage";
 
