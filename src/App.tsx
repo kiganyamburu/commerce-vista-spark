818f1fcbea182f9e63ef41e53731446af549504a
kiganyamburu <mburukiganya@gmail.com>
2025-08-11 21:39:01 +0300

    fix(App): fix a bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index";
-
 import ProductPage from "./pages/ProductPage";
-
 import NotFound from "./pages/NotFound";
-
 import CategoriesPage from "./pages/CategoriesPage";
 
 const queryClient = new QueryClient();
